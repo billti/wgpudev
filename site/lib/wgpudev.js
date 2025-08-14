1ec86ac9a60b8319198c93c55aff8b0fa73aaf5c
Bill Ticehurst <billti@hotmail.com>
2025-08-14 11:39:52 -0700

    Update release bits

--- site/lib/wgpudev.js
+++ site/lib/wgpudev.js
@@ -686,7 +686,7 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper822 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper832 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 49, __wbg_adapter_30);
         return ret;
     };
